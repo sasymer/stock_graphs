82c8a623ee7b46c1e0ea3da4b3b80da1bb3de21c
sasymer <44527156+sasymer@users.noreply.github.com>
2019-11-02 22:36:44 -0400

    Add files via upload

---
+++ src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render () {
+        return (
+            <div style={{backgroundColor: 'blue', width: 200}}>
+                <p>Hallo</p>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
